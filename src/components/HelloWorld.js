834472891ef4b42a1a9fdba6acb1baac922e41e3
Luis Miguel <inf.lrodriguez@gmail.com>
2019-11-02 18:34:59 -0400

    Primer commit: Configuración inicial del proyecto

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+
+export default function HelloWorld() {
+    return (
+        <>
+            <h1>Hello React.js 2</h1>
+        </>
+    )
+}
