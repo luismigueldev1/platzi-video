8d8d16db1971377e125be475c1aa8c825280eb45
Luis Miguel <inf.lrodriguez@gmail.com>
2019-11-14 21:32:02 -0400

    React Hooks: useEffect y useState

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 
 import Header from '../components/Header'
 import Search from '../components/Search'
@@ -11,37 +10,55 @@ import Footer from '../components/Footer'
 import '../assets/styles/App.scss'
 
 export default function App() {
+    const [videos, setVideos] = useState([])
+    
+    useEffect( () => {
+
+       async function fetchData(){
+        try {
+            const response = await fetch("http://localhost:4001/initialState")
+            const data = await response.json()
+            setVideos(data)
+        } catch (error) {
+            console.log(error.message);
+        }
+       }
+       fetchData()
+
+    }, [])
+    console.log(videos);
+    
     return (
         <>
-            <Header/>
-            <Search/>
+            <Header />
+            <Search />
             <Categories title="Recomendados para ti">
                 <Carousel>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
                 </Carousel>
             </Categories>
 
             <Categories title="Tus suscripciones">
                 <Carousel>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
                 </Carousel>
             </Categories>
 
-            <Categories  title="Originales de Platzi Video">
+            <Categories title="Originales de Platzi Video">
                 <Carousel>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
                 </Carousel>
             </Categories>
-            <Footer/>
+            <Footer />
         </>
     )
 }
