caa90f17dea3a972eb401ca48b2f77383764e93d
Luis Miguel <inf.lrodriguez@gmail.com>
2019-11-19 02:13:30 -0400

    Agregando ruta 404 NotFound

---
+++ src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <>
+        <h1>Error 404</h1>
+        <h2>No encontrado! 😥</h2>
+        </>
+    )
+}
--- src/routes/App.js
+++ src/routes/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from '../containers/Home'
 import Login from '../containers/Login'
 import Register from '../containers/Register'
+import NotFound from '../containers/NotFound'
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/login" component={Login}/>
                 <Route exact path="/register" component={Register}/>
+                <Route component={NotFound}/>
             </Switch>
         </BrowserRouter>
     )
