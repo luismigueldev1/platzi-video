10ad22b7d48236a1120cb73bc5a5d7e7d4f5ca1a
Luis Miguel <inf.lrodriguez@gmail.com>
2019-11-24 23:43:19 -0400

    Añadiendo redux developer tools al proyecto

--- src/index.js
+++ src/index.js
@@ -171,7 +171,12 @@ const initialState = {
     ]
 }
 
-const store = createStore(reducer, initialState)
+const store = createStore(
+    reducer, 
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && 
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 
 ReactDOM.render(
     <Provider store={store}>
