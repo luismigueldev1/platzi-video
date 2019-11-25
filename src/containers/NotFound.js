9534f31f3474c0ade99e50a97bd224c73de80af5
Luis Miguel <inf.lrodriguez@gmail.com>
2019-11-25 17:56:37 -0400

    Mejorando container 404 NotFound

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import '../assets/styles/components/NotFound.scss'
 export default function NotFound() {
     return (
-        <>
+        <div className="notfound">
         <h1>Error 404</h1>
         <h2>No encontrado! 😥</h2>
-        </>
+        </div>
     )
 }
